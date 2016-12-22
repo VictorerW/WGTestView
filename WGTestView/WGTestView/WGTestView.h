91d3d69a102008413a67367727260348bfb6e75a
VictorerW <623328266@qq.com>
2016-12-22 11:03:06 +0800

    first
    firstcommit

---
+++ WGTestView/WGTestView/WGTestView.h
@@ -0,0 +1,13 @@
+//
+//  WGTestView.h
+//  WGTestView
+//
+//  Created by cwwmac02 on 16/12/22.
+//  Copyright © 2016年 cwwmac02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WGTestView : UIView
+
+@end
---
+++ WGTestView/WGTestView/WGTestView.m
@@ -0,0 +1,21 @@
+//
+//  WGTestView.m
+//  WGTestView
+//
+//  Created by cwwmac02 on 16/12/22.
+//  Copyright © 2016年 cwwmac02. All rights reserved.
+//
+
+#import "WGTestView.h"
+
+@implementation WGTestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
