d5fa8db63884f684efd07c39f7ba516a5b203771
VictorerW <623328266@qq.com>
2016-12-22 10:59:18 +0800

    Initial Commit

---
+++ WGTestView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WGTestView
+//
+//  Created by cwwmac02 on 16/12/22.
+//  Copyright © 2016年 cwwmac02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WGTestViewTests/WGTestViewTests.m
@@ -0,0 +1,39 @@
+//
+//  WGTestViewTests.m
+//  WGTestViewTests
+//
+//  Created by cwwmac02 on 16/12/22.
+//  Copyright © 2016年 cwwmac02. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WGTestViewTests : XCTestCase
+
+@end
+
+@implementation WGTestViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
